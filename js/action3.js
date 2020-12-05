d6d7215039cd0acc688cabcb0936ca145c8ae007
幼稚的小樱桃 <623383371@qq.com>
2020-12-05 14:34:49 +0800

    all_succeed!!!!!!!!!!!!

--- js/action3.js
+++ js/action3.js
@@ -1,5 +1,5 @@
 function random(min, max) {
-    return Math.floor(Math.random() * (max - min)) === min  ? (min + 1) : Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) === min ? (min + 1) : Math.floor(Math.random() * (max - min)) + min;
 }
 
 function randomMove() {
@@ -7,7 +7,7 @@ function randomMove() {
     var c1 = random(0, 8);
     var r2 = random(0, 8);
     var c2 = random(0, 8);
-    var cell1,cell2;
+    var cell1, cell2;
 
     for (var i = 0; i < 100000; i++) {
         if ((r1 == r2 && c1 == c2) || pieces[r1][c1] == 0 || pieces[r1][c1] == pieces[r2][c2]) {
@@ -17,8 +17,8 @@ function randomMove() {
             c2 = random(0, 8);
         }
     }
-    var cell1 = document.getElementById(getCellID(8, r1, c1));
-    var cell2 = document.getElementById(getCellID(8, r2, c2));
+    cell1 = document.getElementById(getCellID(8, r1, c1));
+    cell2 = document.getElementById(getCellID(8, r2, c2));
     cell1.className = 'white';
     if (pieces[r1][c1] == -1) {
         cell2.className = 'redpiece';
