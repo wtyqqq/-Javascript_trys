64a2eef902d4d14ff82fbf81a0db85e487cbe236
幼稚的小樱桃 <623383371@qq.com>
2020-12-05 11:24:56 +0800

    step_7_fixed_random_move_incomplete

---
+++ js/action3.js
@@ -0,0 +1,21 @@
+function random(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
+}
+
+function randomMove() {
+    var r1 = random(0, 8);
+    var c1 = random(0, 8);
+    var r2 = random(0, 8);
+    var c2 = random(0, 8);
+    var temp = pieces[r1][c1];
+    var cell1 = document.getElementById(getCellID(8, r1, c1));
+    var cell2 = document.getElementById(getCellID(8, r2, c2));
+    for (var i = 0; i < 100000; i++) {
+        if (r1 == r2 && n1 == n1) {
+            r1 = random(0, 8);
+            c1 = random(0, 8);
+            r2 = random(0, 8);
+            c2 = random(0, 8);
+        }
+    }
+}
